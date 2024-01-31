6fb8e71ad03f61c3fe17db6be0fb5dcb7c4b7805
Rakhmat Khaidir <rkhaidir085@gmail.com>
2024-01-31 15:23:16 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+
+  <!-- Bootstrap CSS  -->
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+  <!-- Start Navbar -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container">
+      <img src="assets/img/logo.svg" alt="Bootstrap" width="100">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link" href="#">Testimoni</a>
+        </div>
+        <div class="navbar-nav ms-auto">
+          <a href="#" class="btn btn-primary">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+
+  <!-- Banner -->
+  <div id="banner" class="bg-body-tertiary">
+    <div class="container">
+      <div class="pt-5 text-center">
+        <h1>Search Engine Optimisation & <br/> Marketing.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias at quis eveniet. Nulla impedit veritatis veniam officiis inventore!</p>
+        <img src="assets/img/banner.svg" alt="banner">
+      </div>
+    </div>
+  </div>
+  <!-- End Banner -->
+
+  <!-- Start Feature -->
+  <section id="feature">
+    <div class="container py-5">
+      <h2 class="text-center">How does it works</h2>
+      <p class="mb-5 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt asperiores omnis magni hic animi <br/> aliquam magnam praesentium, quibusdam blanditiis illo.</p>
+
+      <div class="row">
+        <div class="col">
+          <img src="assets/img/feature-1.svg" alt="feature-1">
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos vel fugit molestias commodi. Ex, maiores.</p>
+        </div>
+
+        <div class="col">
+          <img src="assets/img/feature-2.svg" alt="feature-2">
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odit, odio magnam molestiae similique nesciunt!</p>
+        </div>
+
+        <div class="col">
+          <img src="assets/img/feature-3.svg" alt="feature-3">
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus culpa cumque, eligendi tempore quibusdam omnis?</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Feature -->
+
+  <!-- Bootstrap JS -->
+  <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
